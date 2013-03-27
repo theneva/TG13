bf1c135e750f0744729aa16a5cc6475c38085aa7
Mads Røskar <Madshvero@gmail.com>
2013-03-27 23:26:33 +0100

    Added hasher and salt generator <3

---
+++ UselessUtility/src/pw/svn/util/GiefCharInt.java
@@ -0,0 +1,9 @@
+package pw.svn.util;
+
+public class GiefCharInt {
+	public static void main(String[] args) {
+		for(int i = 80; i<= 400; i++){
+			System.out.println(i + ": " + (char) i);
+		}
+	}
+}
--- UselessUtility/src/pw/svn/util/SaltGenerator.java
+++ UselessUtility/src/pw/svn/util/SaltGenerator.java
@@ -1,5 +1,20 @@
 package pw.svn.util;
 
+import java.util.Random;
+
 public class SaltGenerator {
 
+	Random rand;
+	
+	public SaltGenerator(){
+		rand = new Random();
+	}
+	
+	public String getSalt(){
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < 30; i++){
+			sb.append(Integer.toHexString(rand.nextInt(16)));
+		}
+		return sb.toString();
+	}
 }
