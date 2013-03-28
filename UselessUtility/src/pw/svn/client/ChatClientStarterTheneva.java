1b9e50d2b61fa434a75c0a036a98fc15665b06fe
Martin Lehmann <theneva@hotmail.com>
2013-03-28 06:59:18 +0100

    fix?

--- UselessUtility/src/pw/svn/client/ChatClientStarterTheneva.java
+++ UselessUtility/src/pw/svn/client/ChatClientStarterTheneva.java
@@ -1,6 +1,6 @@
 package pw.svn.client;
 
-public class ChatClientStarter {
+public class ChatClientStarterTheneva {
 	public static void main(String[] args) {
 		new ChatClient("151.216.14.39", 8000);
 	}
---
+++ UselessUtility/src/pw/svn/client/ChatClientStarterWaidmann.java
@@ -0,0 +1,7 @@
+package pw.svn.client;
+
+public class ChatClientStarterWaidmann {
+	public static void main(String[] args) {
+		new ChatClient("151.216.14.38", 8000);
+	}
+}
