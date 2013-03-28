0c34bc2861ecda3b8e515e099f8d0f794288217b
Mads Røskar <Madshvero@gmail.com>
2013-03-28 23:14:26 +0100

    Added a GUI for the demo class

--- UselessUtility/src/Demo.java
+++ UselessUtility/src/Demo.java
@@ -1,8 +1,15 @@
 
 
+import java.awt.GridLayout;
 import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Locale;
 import java.util.ResourceBundle;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 import pw.svn.client.ChatClient;
 import pw.svn.server.ChatServer;
@@ -13,35 +20,85 @@ import pw.svn.server.ChatServer;
  *
  */
 public class Demo {
-	public static void main(String[] args) {
-		
-		if (args.length != 2) {
-			new Demo("en", "US");
-		} else {
-			new Demo(args[0], args[1]);
-		}
-	}
+//	public static void main(String[] args) {
+//		
+//		if (args.length != 2) {
+//			new Demo("en", "US");
+//		} else {
+//			new Demo(args[0], args[1]);
+//		}
+//	}
 	
+	private JFrame frame;
+	private JButton btnAddClient, btnAddServer;
+	private JLabel lblClient, lblServer;
+	final String serverAddress = "localhost";
+	final int port;
+	Locale currentLocale;
+	ResourceBundle messages;
 	public Demo(String language, String country) {
 		
-		final String serverAddress = "localhost";
-		final int port = 8000;
+		port = 8000;
+		currentLocale = new Locale(language, country);
+		messages = ResourceBundle.getBundle("MessageBundle", currentLocale);
 		
-		Locale currentLocale = new Locale(language, country);
-		ResourceBundle messages = ResourceBundle.getBundle("MessageBundle", currentLocale);
 		
-		new ChatServer(serverAddress, port, messages);
+		initGUI();
+		initListeners();
+//		new ChatServer(serverAddress, port, messages);
+//		
+//		try {
+//			Thread.sleep(2000);
+//			
+//			for (int i = 0; i < 3; i++) {
+//				new ChatClient(serverAddress, port, messages, new Point(40 + ChatClient.DEFAULT_WIDTH * i, 40));
+//				Thread.sleep(500);
+//			}
+//			
+//		} catch (InterruptedException e) {
+//			e.printStackTrace();
+//		}
+	}
+	public void initGUI(){
+		frame = new JFrame("Pointerless Admin");
+		frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+		frame.setLayout(new GridLayout(2, 2));
 		
-		try {
-			Thread.sleep(2000);
+		btnAddClient = new JButton("Add client");
+		btnAddServer = new JButton("Add server");
+		lblClient = new JLabel("Legg til ny klient: ");
+		lblServer = new JLabel("Legg til ny server: ");
+		
+		frame.add(lblServer);
+		frame.add(btnAddServer);
+		frame.add(lblClient);
+		frame.add(btnAddClient);
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+	}
+	
+	public void initListeners(){
+		btnAddClient.addActionListener(new ActionListener() {
 			
-			for (int i = 0; i < 3; i++) {
-				new ChatClient(serverAddress, port, messages, new Point(40 + ChatClient.DEFAULT_WIDTH * i, 40));
-				Thread.sleep(500);
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				new ChatClient(serverAddress, port, messages, new Point(40 + ChatClient.DEFAULT_WIDTH, 40));
+				
 			}
+		});
+		btnAddServer.addActionListener(new ActionListener() {
 			
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				new ChatServer(serverAddress, port, messages);
+				
+			}
+		});
+		
+	}
+	
+	public static void main(String[] args) {
+		new Demo("no", "NO");
 	}
 }
