a889be5f04e4b997aa486e0a8def57757fa69977
Martin Lehmann <theneva@hotmail.com>
2013-03-28 17:22:41 +0100

    Added internationalization

--- UselessUtility/src/pw/svn/client/ChatClientStarterWaidmann.java
+++ UselessUtility/src/pw/svn/client/ChatClientStarterWaidmann.java
@@ -1,7 +1,25 @@
 package pw.svn.client;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class ChatClientStarterWaidmann {
 	public static void main(String[] args) {
-		new ChatClient("151.216.14.38", 8000);
+		
+		String language, country;
+		
+		if (args.length != 2) {
+			language = new String("en");
+			country = new String("US");
+		} else {
+			language = args[0];
+			country = args[1];
+		}
+		
+		Locale currentLocale = new Locale(language, country);
+		
+		ResourceBundle bundle = ResourceBundle.getBundle("MessageBundle", currentLocale);
+		
+		new ChatClient("151.216.74.138", 8000, bundle);
 	}
 }
--- UselessUtility/src/pw/svn/server/ChatServerStarterTheneva.java
+++ UselessUtility/src/pw/svn/server/ChatServerStarterTheneva.java
@@ -1,17 +1,16 @@
-package pw.svn.client;
+package pw.svn.server;
 
 import java.util.Locale;
 import java.util.ResourceBundle;
 
-public class ChatClientStarterTheneva {
-	
+public class ChatServerStarterTheneva {
 	public static void main(String[] args) {
 		
 		String language, country;
 		
 		if (args.length != 2) {
-			language = new String("en");
-			country = new String("US");
+			language = "en";
+			country = "US";
 		} else {
 			language = args[0];
 			country = args[1];
@@ -19,8 +18,8 @@ public class ChatClientStarterTheneva {
 		
 		Locale currentLocale = new Locale(language, country);
 		
-		ResourceBundle bundle = ResourceBundle.getBundle("MessageBundle", currentLocale);
+		ResourceBundle messages = ResourceBundle.getBundle("MessageBundle", currentLocale);
 		
-		new ChatClient("localhost", 8000, bundle);
+		new ChatServer("localhost", 8000, messages);
 	}
 }
