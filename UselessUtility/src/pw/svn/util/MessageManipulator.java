6231544ee58e25146995897b03e2c27f4d9e5587
Martin Lehmann <theneva@hotmail.com>
2013-03-28 05:45:00 +0100

    R o.o

--- UselessUtility/src/pw/svn/util/MessageManipulator.java
+++ UselessUtility/src/pw/svn/util/MessageManipulator.java
@@ -20,10 +20,9 @@ public class MessageManipulator {
 		String tmp = message;
 		StringBuilder sb = new StringBuilder();
 		while (!done) {
-			int r = rand.nextInt(emoticons.length);
 
 			tmp = tmp.replaceFirst(" ?:-?[a-zA-Z\\(\\)\\[\\]]{1,1} ", (char) 0x4 + " "
-					+ emoticons[r] + " " + (char) 0x3);
+					+ emoticons[rand.nextInt(emoticons.length)] + " " + (char) 0x3);
 			if (tmp.contains((char) 0x3 + "")) {
 				sb.append(hash(tmp.substring(0, tmp.indexOf((char) 0x4))) + tmp.substring(tmp.indexOf((char) 0x4) + 1, tmp.indexOf((char) 0x3)));
 				tmp = tmp.substring(tmp.indexOf((char) 0x3) + 1);
